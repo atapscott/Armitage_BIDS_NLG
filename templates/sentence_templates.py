4b3cadd88806468970eb80b2d089266d1a3f87ee
atapscott <alan.tapscott@gmail.com>
2018-04-22 20:52:23 +0200

    Detached keyword logic and moved to KeyWordTemplateManager

--- templates/sentence_templates.py
+++ templates/sentence_templates.py
@@ -1,6 +1,8 @@
 import string
 import json
 
+from templates.key_word_templates import KeyWordTemplateManager
+
 class CF(string.Formatter):
     def __init__(self):
         super(CF, self).__init__()
@@ -17,20 +19,6 @@ class SentenceTemplateManager:
 
     sentence_templates: dict = json.load(open('templates/sentence_templates.json', 'r'))
 
-    keyword_rendering_filters: dict = {
-        'PLAIN': lambda data_value, field_name: data_value,
-        'dc': lambda data_value, field_name: SentenceTemplateManager.filter_channel_name(field_name, data_value),
-        'dw': lambda data_value, field_name: SentenceTemplateManager.filter_dewar_positioning(data_value)
-    }
-
-    @staticmethod
-    def filter_channel_name(raw_channel_name: str, channel_amount) -> str:
-        return "{} {}{}".format(str(channel_amount), raw_channel_name.replace('ChannelCount', ' channel'),
-                                's' if int(channel_amount) != 1 else '')
-    @staticmethod
-    def filter_dewar_positioning(dewar_positioning: str) -> str:
-        return "positioned {}".format(dewar_positioning)
-
     @classmethod
     def render_key_words(cls, input_data_label: str, input_data: dict) -> str:
         if isinstance(input_data_label, str) and '_' not in input_data_label:
@@ -39,8 +27,8 @@ class SentenceTemplateManager:
             field_name: str = input_data_label.split('_')[-1]
             sub_filter_name: str = input_data_label.split('_')[-2]
             input_data_value: str = cls.render_key_words(field_name, input_data)
-            key_word_filter = cls.keyword_rendering_filters[sub_filter_name]
-            return key_word_filter(input_data_value, field_name)
+            key_word_filter = KeyWordTemplateManager.get_key_word_filter(sub_filter_name)
+            return key_word_filter(field_name=field_name, data_value=input_data_value, input_data=input_data)
 
     # TODO just iterating the sentence documente. Should include proper report templates to manage the macro structure.
     @classmethod
